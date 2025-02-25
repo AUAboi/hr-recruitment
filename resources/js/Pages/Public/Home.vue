98e5ed04dedfa382b1ae6371a37397c3072a8f61
AwaisFaisal <112121922+AwaisFaisal@users.noreply.github.com>
2025-02-25 23:45:31 +0500

    name change

--- resources/js/Pages/Public/Home.vue
+++ resources/js/Pages/Public/Home.vue
@@ -7,12 +7,20 @@ import PrimaryButton from "@/Components/PrimaryButton.vue";
     <div class="flex items-center justify-evenly text-white">
         <div class="max-w-xl">
             <h2 class="text-5xl mb-3">
-                Meet CORE - your recruitment assistant from the future
+                Meet Job Nexus - your career assistant from the future
             </h2>
             <p class="text-gray-200 text-xl pb-10">
-                Completely automate research, sourcing, and all your other
-                recruiting bottlenecks. Focus on your client and high touch
-                candidates.
+                Completely automate your job search and recruitment needs with
+                Job Nexus. We are a global recruitment company that provides a
+                unique service to job seekers and employers. We are a team of
+                professionals who are passionate about helping you find the
+                right job or the right candidate. We have a proven track record
+                of success in the recruitment industry and we are confident that
+                we can help you achieve your goals. Our mission is to make the
+                recruitment process as simple and efficient as possible for both
+                job seekers and employers. We are committed to providing you
+                with the best possible service and we look forward to working
+                with you.
             </p>
 
             <PrimaryButton
