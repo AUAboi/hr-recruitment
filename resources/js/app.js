56d8a904d1cd5e2476a4145dec5b31a1a88111a2
Ahsan Faisal <auawgamers@gmail.com>
2025-02-26 12:56:55 +0500

    tes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,28 +1,31 @@
-import './bootstrap';
-import '../css/app.css';
+import "./bootstrap";
+import "../css/app.css";
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
 import axios from "axios";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
-import RecruiterLayout from "@/Layouts/RecruiterLayout.vue"
-import PublicLayout from "@/Layouts/PublicLayout.vue"
+import RecruiterLayout from "@/Layouts/RecruiterLayout.vue";
+import PublicLayout from "@/Layouts/PublicLayout.vue";
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL + '/api'
+axios.defaults.baseURL = "https://job-nexus.mundanedev.com" + "/api";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: async name => {
-        const page = await resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue'))
+    resolve: async (name) => {
+        const page = await resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/**/*.vue")
+        );
         if (page.default.layout === undefined) {
             if (name.startsWith("Recruiter")) {
                 page.default.layout = RecruiterLayout;
             } else if (name.startsWith("Public")) {
-                page.default.layout = PublicLayout
+                page.default.layout = PublicLayout;
             }
         }
         return page;
@@ -34,6 +37,6 @@ createInertiaApp({
             .mount(el);
     },
     progress: {
-        color: '#ff8d4e',
+        color: "#ff8d4e",
     },
 });
